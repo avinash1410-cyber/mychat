4c699e688607490e4816ef83963e05a150a6fe9c
Avinash Kumar <avi8654340@gmail.com>
2022-07-19 01:23:47 +0530

    First version of application moved into GitHub

---
+++ mychat/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+
+
+from django.urls import include, path, re_path
+from django.views.static import serve
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls')),
+    ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # to import static in deployment
